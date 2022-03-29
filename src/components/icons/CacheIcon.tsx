22bea9e906be72096616bae87f36fae9a4ad3fe2
Esther Alvarez Feijoo <ealvarez@cloudbees.com>
2022-03-29 23:35:06 +0200

    Remove deadcode and use only HUI icons

--- src/components/icons/CacheIcon.tsx
+++
@@ -1,6 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLaptopHouse as icon } from '@fortawesome/free-solid-svg-icons'
-
-export const CacheIcon = () => {
-    return <FontAwesomeIcon icon={icon} className="cacheIcon" />
-}
--- src/components/icons/ErrorIcon.tsx
+++
@@ -1,6 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBomb as icon } from '@fortawesome/free-solid-svg-icons'
-
-export const ErrorIcon = () => {
-    return <FontAwesomeIcon icon={icon} className="errorIcon" />
-}
